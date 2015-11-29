7d4c82f2ed476af65899693cda4a7b6862ab571c
m.taira <taira.masaru@gmail.com>
2015-11-29 18:33:37 +0900

    chapter3 05 screenshot-app

---
+++ chapter3/05/screenshot-app/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var TARGET_URL = 'http://atom.io';
+
+var app = require('app');
+var BrowserWindow = require('browser-window');
+var fs = require('fs');
+
+var win = null;
+app.on('ready', function(){
+  win = new BrowserWindow({width: 1024, width:800});
+  win.loadUrl(TARGET_URL);
+  win.webContents.on('did-finish-load', captureFunc)
+});
+
+function captureFunc(){
+  win.capturePage(function(img){
+    fs.writeFileSync('screenshot.png', img.toPng());
+  })
+}
